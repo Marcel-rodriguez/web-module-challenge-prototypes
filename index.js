2fbc0e6c7fff70629d293d11840cc7e75e799503
marcel <marcel_rodrgez@yahoo.com>
2022-12-14 22:02:12 -0500

    Passing all tests

--- index.js
+++ index.js
@@ -79,9 +79,18 @@ Car.prototype.drive = function(distance){
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, toy) {
+Person.call(this, name, age)
+this.favoriteToy = toy
 }
+
+  Baby.prototype = Object.create(Person.prototype)
+
+ Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+ }
+
+
 
 
 /* 
